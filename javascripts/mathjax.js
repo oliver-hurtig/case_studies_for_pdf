d05901b9625d2e3accc502e6693dcfd2a5b9ee41
Oliver Hurtig <oliver.hurtig@ec.europa.eu>
2024-10-15 17:24:12 +0200

    Deployed 54b4b0e with MkDocs version: 1.6.1

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -17,8 +17,9 @@ window.MathJax = {
   };
   
   document$.subscribe(() => { 
-  
-    //MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+
+    // there is some strange behaviour of MathJax not correctly loading unicode after a page reload when MathJax is still in cache. The first line here throws an error, but then the rest works (it's probably just because it slows down the page load and might be better handled by waiting for page load)
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
     MathJax.startup.output.clearCache()
     MathJax.typesetClear()
     MathJax.texReset()
